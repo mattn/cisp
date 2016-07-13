bb3064e839f55c9412a00d9226b90ce08f7e7694
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-07-13 10:20:56 +0000

    fix fizzbuzz.lisp

--- example/fizzbuzz.lisp
+++ example/fizzbuzz.lisp
@@ -1,8 +1,10 @@
-(dotimes (i 100)
-  (println
-   (cond
-    ((= 0 (mod i 15)) "FizzBuzz")
-    ((= 0 (mod i 3)) "Fizz")
-    ((= 0 (mod i 5)) "Buzz")
-    ('t i))))
-  
+(defun fizzbuzz ()
+  (dotimes (i 100)
+    (println
+     (cond
+      ((= 0 (mod i 15)) "FizzBuzz")
+      ((= 0 (mod i 3)) "Fizz")
+      ((= 0 (mod i 5)) "Buzz")
+      ('t i)))))
+
+(fizzbuzz)
