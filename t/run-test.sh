b6c95fb28da04295a1ed95ce4dd55597cca8b7a1
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-07-22 03:01:10 +0000

    fix test runner

--- t/run-test.sh
+++ t/run-test.sh
@@ -7,6 +7,8 @@ if [ "x$1" != "x" ]; then
   FILTER=$1
 fi
 
+export CISP_BASE_DIR=..
+
 /bin/ls *.lisp | grep $FILTER | /usr/bin/sort | while read file; do
   echo -n "$file: "
   if [ -e `basename $file .lisp`.skip ]; then
