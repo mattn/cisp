99c483805a4d9e327220eb2b1d7fc8e16a77e8bb
Mitsuru Kariya <kariya_mitsuru@hotmail.com>
2016-07-26 11:04:49 +0900

    fix or test

--- t/32-or.lisp
+++ t/32-or.lisp
@@ -1,6 +1,8 @@
-(defun 1-or-2 (x) (and (>= x 1) (<= x 2)))
+(defun 1-or-3 (x) (or (= x 1) (= x 3)))
 
-(print (1-or-2 0))
-(print (1-or-2 1))
-(print (1-or-2 2))
+(print (1-or-3 0))
+(print (1-or-3 1))
+(print (1-or-3 2))
+(print (1-or-3 3))
+(print (1-or-3 4))
 
